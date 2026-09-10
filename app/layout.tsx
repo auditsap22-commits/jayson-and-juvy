60c928e51c8b04570747cf23e685d531cf2a35cd
Rolando Valle <rolandovalle0428@gmail.com>
2026-09-10 19:09:48 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { anastasiaScript } from "@/lib/fonts"
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://jayson-and-juvy.weddinginvitationrsvp.com/"
 const canonicalUrl = siteUrl.replace(/\/$/, "")
-  const desktopHero = "/Details/linkPreviewnew.png"
-const mobileHero = "/Details/linkPreviewnew.png"
+  const desktopHero = "/Details/new_linkePreview.png"
+const mobileHero = "/Details/new_linkPreview.png"
 const eventImageUrl = `${canonicalUrl}${desktopHero}`
 const OG_IMAGE_FALLBACK = `${canonicalUrl}${desktopHero}`
 
